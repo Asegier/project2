5ea9e358277aeaa9e9eed7e5a8a0a58db98cc9b2
Chris Chan <cjaysea@gmail.com>
2017-03-16 21:51:03 +0800

    got new page sorted

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var request = require('request');
-var cheerio = require('cheerio');
-var fs = require('fs');
+var express 		= require('express');
+var path 			= require('path');
+var favicon 		= require('serve-favicon');
+var logger 			= require('morgan');
+var cookieParser 	= require('cookie-parser');
+var bodyParser 		= require('body-parser');
+var request 		= require('request');
+var cheerio 		= require('cheerio');
+var fs 				= require('fs');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
-var scraper = require('./utils/scraper');
+var index 			= require('./routes/index');
+var users 			= require('./routes/users');
+var movieGrabber 	= require('./routes/page')
+var scraper 		= require('./utils/scraper');
 
-var app = express();
+var app 			= express();
 
 // Connect with MongoDB
-var mongoose = require('mongoose');
+var mongoose 		= require('mongoose');
 mongoose.connect('mongodb://localhost/project_2');
 
 // view engine setup
@@ -32,122 +33,55 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-
-var Cinema = require('./models/cinemas');
-var getUACinemas = function (url){
-	var totalcinemas = []
-	request(url, function(error, response, html){s
-		console.log("REQUEST")
-		if(!error){
-			var C = cheerio.load(html);
-
-			var title, release, rating;
-			var json = { title : ""};
-			C('.cinemas').filter(function(){
-				
-				/*
-				var data = C(this);
-				title = data.children().children().text()
-				json.title = title;
-				
-				console.log("title", data.children().children());
-				
-				*/
-				
-				var data = C('.cinemas ul li');
-				for(i = 0; i < C(data).length; i++ ){
-					var cinema = new Cinema();
-					cinema.name = C(data[i]).text();
-					cinema.save();
-					console.log(C(data[i]).text());
-				}
-//				console.log(C(data).length)
-				
-				totalcinemas.push(json.title)
-			})
-		}
-//		console.log(totalcinemas)
-			var titlesArr = []
-			for(i = 0; i < totalcinemas.length; i++){
-				if(!titlesArr.includes(totalcinemas[i])){
-					titlesArr.push(totalcinemas[i])
-				}
-			}
-//		console.log(titlesArr)
-			console.log('CINEMASSSS');
-//		})
-	})
-}
-
-//getUACinemas('http://www.uacinemas.com.hk/eng/main/HomePage')
-getUACinemas('http://www4.mclcinema.com/index.aspx?visLang=2')
+app.use('/page', movieGrabber)
 
 
-var express = require('express');
-var router = express.Router();
+//	scraper.getMCLCinemas('http://www4.mclcinema.com/MCLWebAPI2/GetCinemaDetails.aspx?l=2')
+			
+//scraper.getUACinemas('http://www.uacinemas.com.hk/eng/main/HomePage')
+//getUACinemas('http://www4.mclcinema.com/index.aspx?visLang=2')
 
-var Movie = require('./models/movies')
-var getUAMovies = function (url){
-	var totaltitles = []
-	request(url, function(error, response, html){
-		console.log("REQUEST")
-		if(!error){
-			var C = cheerio.load(html);
+var CronJob = require('cron').CronJob;
+new CronJob('01 * * * * *', function() {
+  console.log('You will see this message every 5 seconds');
+}, null, true, 'Asia/Hong_Kong');
 
-			var title, release, rating;
-			var json = { title : ""};
-			C('.center_info').filter(function(){
-				var data = C(this);
-				title = data.children().first().text();
-
-				json.title = title;
-				totaltitles.push(json.title)
-				
-			})
-		}
-		console.log(totaltitles)
-			var titlesArr = []
-			for(i = 0; i < totaltitles.length; i++){
-				if(!titlesArr.includes(totaltitles[i])){
-					titlesArr.push(totaltitles[i])
-					var movie = new Movie();
-					movie.name = totaltitles[i]
-					movie.save()
-				}
-			}
-		console.log(titlesArr)
-			console.log('File successfully written! - Check your project directory for the output.json file');
-//		})
-	})
-}
-
-const UAcwb = 'https://www.uacinemas.com.hk/eng/cinemas/Cinemas?id=4';
-const UAiMaxmk = 'http://www.uacinemas.com.hk/eng/cinemas/Cinemas?id=24';
-const UAiMaxklnbay = 'http://www.uacinemas.com.hk/eng/cinemas/Cinemas?id=15';
-const UAiMaxtst = 'http://www.uacinemas.com.hk/eng/cinemas/Cinemas?id=19';
-const UAPCtst = 'http://www.uacinemas.com.hk/eng/cinemas/Cinemas?id=25';
-const UAmk = 'http://www.uacinemas.com.hk/eng/cinemas/Cinemas?id=23';
-const UAtst = 'http://www.uacinemas.com.hk/eng/cinemas/Cinemas?id=18';
-const UAklnbay = 'http://www.uacinemas.com.hk/eng/cinemas/Cinemas?id=14';
-const UAtuenmun = 'http://www.uacinemas.com.hk/eng/cinemas/Cinemas?id=17';
-const UAshatin = 'http://www.uacinemas.com.hk/eng/cinemas/Cinemas?id=1';
-const UAairport = 'http://www.uacinemas.com.hk/eng/cinemas/Cinemas?id=22'
-//const MCL = ;
-//const BROAD = ;
-
+const UAcwb 		= 'https://www.uacinemas.com.hk/eng/cinemas/Cinemas?id=4';
+const UAiMaxmk 		= 'http://www.uacinemas.com.hk/eng/cinemas/Cinemas?id=24';
+const UAiMaxklnbay 	= 'http://www.uacinemas.com.hk/eng/cinemas/Cinemas?id=15';
+const UAiMaxtst 	= 'http://www.uacinemas.com.hk/eng/cinemas/Cinemas?id=19';
+const UAPCtst 		= 'http://www.uacinemas.com.hk/eng/cinemas/Cinemas?id=25';
+const UAmk 			= 'http://www.uacinemas.com.hk/eng/cinemas/Cinemas?id=23';
+const UAtst 		= 'http://www.uacinemas.com.hk/eng/cinemas/Cinemas?id=18';
+const UAklnbay 		= 'http://www.uacinemas.com.hk/eng/cinemas/Cinemas?id=14';
+const UAtuenmun 	= 'http://www.uacinemas.com.hk/eng/cinemas/Cinemas?id=17';
+const UAshatin 		= 'http://www.uacinemas.com.hk/eng/cinemas/Cinemas?id=1';
+const UAairport 	= 'http://www.uacinemas.com.hk/eng/cinemas/Cinemas?id=22';
+//const MCL 		= ;
+//const BROAD 		= ;
+debugger;
 /* Cinema & Movies
 Feedback all the Cinemas in a given location
 */
 
 app.get('/scrape', function(req, res){
 
-	getUAMovies(UAcwb)
+	scraper.getUAMovies(UAcwb);
+	scraper.getUAMovies(UAiMaxmk);
+	
 	
 	res.send('Check your console!')
 
 })
 
 
+
+app.get('/toobad', function(req, res){
+	res.send('Please contact the nearest Mental Facility near you.')
+})
+
+var Movie = require('./models/movies')
+//console.log(Movie.find('name'))
 
 
 // catch 404 and forward to error handler
--- utils/scraper.js
+++ utils/scraper.js
@@ -1,60 +1,96 @@
-var express = require('express');
-var router = express.Router();
+var Cinema = require('../models/cinemas');
+var request = require('request');
+var cheerio = require('cheerio');
+var Movie = require('../models/movies');
 
-router.get('/', function(req, res){
-
-	url = 'https://www.uacinemas.com.hk/eng/cinemas/Cinemas?id=4';
-
+exports.getUACinemas = function(url){
+console.log("HELLO!!! WE GOT THE SCRAPER!!!!")
+	var totalcinemas = []
 	request(url, function(error, response, html){
 		console.log("REQUEST")
 		if(!error){
 			var C = cheerio.load(html);
 
 			var title, release, rating;
-			var json = { title : "", release : "", rating : ""};
+			var json = { title : ""};
+			C('.cinemas').filter(function(){
+//				var data = C(this)
+//					console.log(JSON.parse(data))
+				/*
+				var data = C(this);
+				title = data.children().children().text()
+				json.title = title;
+
+				console.log("title", data.children().children());
+
+				*/
+				var data = C('.cinemas ul li');
+				for(i = 0; i < C(data).length; i++ ){
+					var cinema = new Cinema();
+					cinema.name = C(data[i]).text();
+					cinema.save();
+					console.log(C(data[i]).text());
+				}
+			})
+		}
+//		console.log(totalcinemas)
+//			var titlesArr = []
+//			for(i = 0; i < totalcinemas.length; i++){
+//				if(!titlesArr.includes(totalcinemas[i])){
+//					titlesArr.push(totalcinemas[i])
+//				}
+//		}
+//		console.log(titlesArr)
+			console.log('CINEMASSSS');
+	})
+}
+
+exports.getMCLCinemas = function (url){
+	request(url, function(error, response, html){
+		if(!error){
+			var data = JSON.parse(html);
+			for(i = 0; i < data.length; i++){
+				var cinema = new Cinema();
+				cinema.name = data[i].n;
+				cinema.address = data[i].a;
+				cinema.save();
+				console.log("Name: " + cinema.name, "Address: " + cinema.address)
+			}
+		}
+	})
+}
+
+
+exports.getUAMovies = function (url){
+	var totaltitles = []
+	request(url, function(error, response, html){
+		console.log("REQUEST")
+		if(!error){
+			var C = cheerio.load(html);
+
+			var title, release, rating;
+			var json = { title : ""};
 			C('.center_info').filter(function(){
 				var data = C(this);
 				title = data.children().first().text();
 
-				//				release = data.children().last().children().text();
-
 				json.title = title;
-				totaltitles = [json]
-				console.log(totaltitles)
-				//				json.release = release;
+				totaltitles.push(json.title)
+				
 			})
-
-			// Since the rating is in a different section of the DOM, we'll have to write a new jQuery filter to extract this information.
-
-			//			$('.star-box-giga-star').filter(function(){
-			//				var data = $(this);
-			//
-			//				// The .star-box-giga-star class was exactly where we wanted it to be.
-			//				// To get the rating, we can simply just get the .text(), no need to traverse the DOM any further
-			//
-			//				rating = data.text();
-			//
-			//				json.rating = rating;
-			//			})
 		}
-//		fs.writeFile('output.json', JSON.stringify(json, null, 1), function(err){
-//			var titlesArr = []
-//			for(i = 0; i < json.length; i++){
-//				if(titlesArr.includes(json[i].title)){
-//					titlesArr.push(json[i].title)
-//				}
-//			
-//			}
+		console.log(totaltitles)
+			var titlesArr = []
+			for(i = 0; i < totaltitles.length; i++){
+				if(!titlesArr.includes(totaltitles[i])){
+					titlesArr.push(totaltitles[i])
+					var movie = new Movie();
+					movie.name = totaltitles[i]
+					movie.save()
+				}
+			}
+		console.log(titlesArr)
 			console.log('File successfully written! - Check your project directory for the output.json file');
 //		})
 	})
-
-
-	
-
-	// Finally, we'll just send out a message to the browser reminding you that this app does not have a UI.
-	res.send('Check your console!')
-
-})
-
-//module.exports = router;
+}
